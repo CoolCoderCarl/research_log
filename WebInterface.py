0967b1daa6bf0665417e3429cffb75b91232f90f
CoolCoderCarl <123qwekir2wl@gmail.com>
2021-10-12 12:50:47 +0300

    Add flake8; fix according PEP8 recommendations

--- WebInterface.py
+++ WebInterface.py
@@ -1,39 +1,44 @@
+from datetime import datetime
 from flask import Flask
 from flask import Response
-from flask import url_for
 from flask import request
-from datetime import datetime
 from flask import render_template
+from flask import url_for
 
 now = datetime.now()
 dt_string = now.strftime("%d.%m.%Y_%H.%M.%S")
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Index')
+
 
 @app.route('/submit', methods=['POST'])
 def submit():
     text = request.form['text']
     with open("%s.txt" % dt_string, "w") as text_file:
         text_file.write(text)
-    ### DEDICATE FUNC
-    ### CLOSE WORK WITH FILE
+    # DEDICATE FUNC
+    # CLOSE WORK WITH FILE
     return render_template('index.html', title='Index')
 
-### URL SAME AS INDEX
+
+# URL SAME AS INDEX
 @app.route('/time_feed')
 def time_feed():
     def generate():
         yield datetime.now().strftime("%Y.%m.%d|%H:%M:%S")
     return Response(generate(), mimetype='text')
 
+
 @app.route('/projects')
 def projects():
     return 'The project page'
+
 
 @app.route('/about')
 def about():
