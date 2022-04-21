44900e30a8af2c0b6bc4942f3b3651f040f6396f
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-04-21 13:21:37 +0300

    Reformat name

--- webapp.py
+++ webapp.py
@@ -10,36 +10,35 @@ dt_string = now.strftime("%d.%m.%Y_%H.%M.%S")
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
 def index():
-    return render_template('index.html', title='Index')
+    return render_template("index.html", title="Index")
 
 
-@app.route('/submit', methods=['POST'])
+@app.route("/submit", methods=["POST"])
 def submit():
-    text = request.form['text']
+    text = request.form["text"]
     with open("%s.txt" % dt_string, "w") as text_file:
         text_file.write(text)
-    return render_template('index.html', title='Index')
+    return render_template("index.html", title="Index")
 
 
-@app.route('/projects')
+@app.route("/projects")
 def projects():
-    return 'The project page'
+    return "The project page"
 
 
-@app.route('/about')
+@app.route("/about")
 def about():
-    return 'The about page'
+    return "The about page"
 
 
 with app.test_request_context():
-    print(url_for('index'))
+    print(url_for("index"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     # app.run(host='0.0.0.0',port=5000, debug=True) # DOCKER
     app.run(port=5000, debug=True)
-
