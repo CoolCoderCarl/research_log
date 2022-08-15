3e5496544dd56bc9403a7154bb4984c83e76f25e
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-08-15 18:28:29 +0300

    Updated

--- webapp.py
+++ webapp.py
@@ -59,5 +59,5 @@ with app.test_request_context():
 
 
 if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port=5000, debug=True)  # DOCKER
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)  # DOCKER
+    # app.run(port=5000, debug=True)
